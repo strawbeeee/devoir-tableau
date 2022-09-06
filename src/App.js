f87e03a3cb8da7b0ac6b9a5a161df6954c823da1
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2022-09-06 08:01:06 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/**
+ * @module App
+ */
+export default class App {
+	/**
+	 * Méthode principale. Sera appelée après le chargement de la page.
+	 */
+	static main() {
+		console.log("Je suis prêt");
+		var app = document.getElementById("app");
+		// app.innerHTML = "La page est chargée";
+	}
+	/**
+	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
+	 * @returns undefined Ne retourne rien
+	 */
+	static init() {
+		window.addEventListener("load", () => {
+			this.main();
+		});
+	}
+}
+App.init();
